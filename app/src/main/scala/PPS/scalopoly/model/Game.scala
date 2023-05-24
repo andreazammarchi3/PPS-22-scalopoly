e9b2889014716c4bf6bc64a4011943b51bcdd2cb
Andrea Negri <andrea.negri4@studio.unibo.it>
2023-05-24 14:08:37 +0200

    Updated Game model with getter and setter

--- app/src/main/scala/PPS/scalopoly/model/Game.scala
+++ app/src/main/scala/PPS/scalopoly/model/Game.scala
@@ -2,9 +2,28 @@ package PPS.scalopoly.model
 
 class Game:
 
-  var currentPlayer: Option[Player] = None
-  var players: List[Player] = List.empty
-  var gameBoard: Map[Integer, SpaceName] = Map.empty
+  private[this] var _currentPlayer: Option[Player] = None
+  private[this] var _players: List[Player] = List.empty
+  private[this] var _gameBoard: Map[Integer, SpaceName] = Map.empty
+
+  def currentPlayer: Option[Player] = _currentPlayer
+
+  def currentPlayer_=(value: Option[Player]): Unit = {
+    _currentPlayer = value
+  }
+
+  def players: List[Player] = _players
+
+  def players_=(value: List[Player]): Unit = {
+    _players = value
+  }
+
+  def gameBoard: Map[Integer, SpaceName] = _gameBoard
+
+  def gameBoard_=(value: Map[Integer, SpaceName]): Unit = {
+    _gameBoard = value
+  }
 
   def addPlayer(newPlayer: Player): Unit =
     players = newPlayer :: players
+
