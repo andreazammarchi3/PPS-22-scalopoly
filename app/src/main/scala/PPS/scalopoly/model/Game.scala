2a66a6c39c39be7c825b1f120e37ca84de5b2769
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-05-25 16:54:38 +0200

    Cherry-pick from 68c52e2

--- app/src/main/scala/PPS/scalopoly/model/Game.scala
+++ app/src/main/scala/PPS/scalopoly/model/Game.scala
@@ -1,10 +1,14 @@
 package PPS.scalopoly.model
 
-class Game:
+import PPS.scalopoly.utils.GameUtils
 
-  private[this] var _currentPlayer: Option[Player] = None
-  private[this] var _players: List[Player] = List.empty
-  private[this] val _gameBoard: GameBoard = new GameBoard
+import scala.util.Random
+
+class Game:
+  private var _currentPlayer: Option[Player] = None
+  private var _players: List[Player] = List.empty
+  private val _gameBoard: GameBoard = new GameBoard
+  private val dice = new Dice
 
   def currentPlayer: Option[Player] = _currentPlayer
 
@@ -23,3 +27,15 @@ class Game:
   def addPlayer(newPlayer: Player): Unit =
     players = newPlayer :: players
 
+  def startGame(): Unit =
+    players = GameUtils.shufflePlayers(players)
+    currentPlayer = Some(players.head)
+
+  def endTurn(): Unit =
+    val currentIndex = players.indexOf(currentPlayer.get)
+    val newIndex = (currentIndex + 1) % players.length
+    currentPlayer = Some(players(newIndex))
+
+  def movePlayer(): Unit =
+    currentPlayer.get.actualPosition = 
+      GameUtils.addSumToPosition(dice.sum(), currentPlayer.get.actualPosition, gameBoard)
