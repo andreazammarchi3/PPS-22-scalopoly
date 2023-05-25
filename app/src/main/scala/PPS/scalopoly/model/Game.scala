ef0cccb3075e573e8f6a21bdc3eae19f5d441b3d
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-05-25 15:13:56 +0200

    Cherry-pick from 02f0bc0

--- app/src/main/scala/PPS/scalopoly/model/Game.scala
+++ app/src/main/scala/PPS/scalopoly/model/Game.scala
@@ -4,7 +4,7 @@ class Game:
 
   private[this] var _currentPlayer: Option[Player] = None
   private[this] var _players: List[Player] = List.empty
-  private[this] var _gameBoard: Map[Int, SpaceName] = Map.empty
+  private[this] val _gameBoard: GameBoard = new GameBoard
 
   def currentPlayer: Option[Player] = _currentPlayer
 
@@ -17,12 +17,8 @@ class Game:
   def players_=(value: List[Player]): Unit = {
     _players = value
   }
-
-  def gameBoard: Map[Int, SpaceName] = _gameBoard
-
-  def gameBoard_=(value: Map[Int, SpaceName]): Unit = {
-    _gameBoard = value
-  }
+  
+  def gameBoard: GameBoard = _gameBoard
 
   def addPlayer(newPlayer: Player): Unit =
     players = newPlayer :: players
