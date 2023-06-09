5c999264f167a1ad4047ba0d77c5e59b12eb11ab
Andrea Negri <andrea.negri4@studio.unibo.it>
2023-06-09 11:21:16 +0200

    Imported token img and created their enum values

--- app/src/main/scala/PPS/scalopoly/utils/resources/ImgResources.scala
+++ app/src/main/scala/PPS/scalopoly/utils/resources/ImgResources.scala
@@ -2,3 +2,11 @@ package PPS.scalopoly.utils.resources
 
 enum ImgResources(path: String):
   case GAMEBOARD extends ImgResources("/img/Gameboard.png")
+  case BOAT_TOKEN extends ImgResources("/img/token/Boat.png")
+  case BOOT_TOKEN extends ImgResources("/img/token/Boot.png")
+  case CAR_TOKEN extends ImgResources("/img/token/Car.png")
+  case CAT_TOKEN extends ImgResources("/img/token/Cat.png")
+  case DOG_TOKEN extends ImgResources("/img/token/Dog.png")
+  case HAT_TOKEN extends ImgResources("/img/token/Hat.png")
+  case THIMBLE_TOKEN extends ImgResources("/img/token/Thimble.png")
+  case WHEELBARROW_TOKEN extends ImgResources("/img/token/Wheelbarrow.png")
