1aa9c0dbfb29ab799afa8267a15976d39d8ae771
Michele Laddaga <michele.laddaga@studio.unibo.it>
2023-06-15 19:00:04 +0200

    merged from feature/gui

---
+++ app/src/main/scala/PPS/scalopoly/controller/ConfigurationMenuController.scala
@@ -0,0 +1,13 @@
+package PPS.scalopoly.controller
+
+import java.net.URL
+import java.util
+
+class ConfigurationMenuController extends jfxf.Initializable:
+
+  override def initialize(url: URL, rb: util.ResourceBundle): Unit =
+    var a = 1
+
+
+  def playGame(): Unit =
+    var b = 1
