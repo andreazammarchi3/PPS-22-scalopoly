b1029635824fbb389bd00a2d6159dace74110671
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-06-29 12:14:24 +0200

    Create SpaceGroup

--- app/src/main/scala/PPS/scalopoly/model/SpaceGroup.scala
+++ app/src/main/scala/PPS/scalopoly/model/SpaceGroup.scala
@@ -1,14 +1,13 @@
 package PPS.scalopoly.model
 
-enum SpaceGroup(name: String):
-  case ROSA extends SpaceGroup("Gruppo Rosa")
-  case CELESTE extends SpaceGroup("Gruppo Celeste")
-  case ARANCIONE extends SpaceGroup("Gruppo Arancione")
-  case MARRONE extends SpaceGroup("Gruppo Marrone")
-  case ROSSO extends SpaceGroup("Gruppo Rosso")
-  case GIALLO extends SpaceGroup("Gruppo Giallo")
-  case VERDE extends SpaceGroup("Gruppo Verde")
-  case VIOLA extends SpaceGroup("Gruppo Viola")
-  case COMPAGNIA_ELETTRICA extends SpaceGroup("Gruppo Compagnia Elettrica")
-  case FERROVIARIO extends SpaceGroup("Gruppo Ferroviario")
-  case NO_GROUP extends SpaceGroup("")
+enum SpaceGroup:
+  case ROSA
+  case CELESTE
+  case ARANCIONE
+  case MARRONE
+  case ROSSO
+  case GIALLO
+  case VERDE
+  case VIOLA
+  case SOCIETA
+  case FERROVIARIO
