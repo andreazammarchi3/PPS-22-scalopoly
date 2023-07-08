48d6cb287a210f81421792b534bf9cf779ee8152
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-07-08 12:15:38 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ val jacocoExcludes = listOf("**/FxmlUtils**", "**/AlertUtils**")
 tasks.jacocoTestReport {
     reports {
         xml.required.set(false)
-        csv.required.set(false)
+        csv.required.set(true)
         html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
     }
     classDirectories.setFrom(
